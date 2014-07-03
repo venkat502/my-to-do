9e25184fbedd7e12cfa2e98d4735fc138eb95c39
venkat502 <noreply>
2014-07-03 12:01:15 +0000

    Add component manuals

---
+++ ui/manuals.reel/manuals.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/manuals.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Manuals
+ * @extends Component
+ */
+exports.Manuals = Component.specialize(/** @lends Manuals# */ {
+    constructor: {
+        value: function Manuals() {
+            this.super();
+        }
+    }
+});
