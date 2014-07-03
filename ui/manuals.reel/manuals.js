5d41f898e8b9fea81a23160887d7236b14b5d224
venkat502 <noreply>
2014-07-03 12:02:14 +0000

    Update components

--- ui/manuals.reel/manuals.js
+++ ui/manuals.reel/manuals.js
@@ -8,10 +8,60 @@ var Component = require("montage/ui/component").Component;
  * @class Manuals
  * @extends Component
  */
-exports.Manuals = Component.specialize(/** @lends Manuals# */ {
-    constructor: {
-        value: function Manuals() {
-            this.super();
+exports.Manuals = Component.specialize( /** @lends Manuals# */ {
+    handleAction: {
+        value: function() {
+            var manuals = this.templateObjects.manuals;
+            searchtext = $("#search-text").val();
+            catid = $("#category").val();
+            pageNumber = $("#pageNumber").val();
+            limit = $("#limit").val();
+
+            if (catid == null) {
+                catid = "*";
+            }
+
+            if (pageNumber == null) {
+                pageNumber = 1;
+            }
+
+            if (limit == null) {
+                limit = 20;
+            }
+
+
+            $("#searchform").submit(function(e) {
+                e.preventDefault();
+                $.ajax({
+                    url: 'http://vman.ngrok.com/_api/manuals/search.json',
+                    type: 'get',
+                    data: {
+                        searchText: searchtext,
+                        catid: catid,
+                        pn: pageNumber,
+                        limit: limit
+                    }
+                }).done(function(data) {
+                    console.log("success");
+                    console.log(data.code);
+                    console.log(data.message);
+                    var myarr = [];
+                    if (data.data.hits.hits.length > 0) {
+                        for (i = 0; i < data.data.hits.hits.length; i++) {
+                            myarr[i] = {
+                                "manual": data.data.hits.hits[i]
+                            };
+                        }
+                        manuals.content = myarr;
+                    } else {
+                        $(".results").empty().append("No Results Found");
+                    }
+                }).fail(function(data) {
+                    console.log("error");
+                }).always(function() {
+                    console.log("complete");
+                });
+            });
         }
     }
 });
